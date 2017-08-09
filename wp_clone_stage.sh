98e78b9f14de1535ba6fbcd4a14b23901c94315f
Jason <jasonventresca@gmail.com>
2017-08-09 01:59:57 +0000

    OK, the tool is actually functional, now.

--- wp_clone_stage.sh
+++ wp_clone_stage.sh
@@ -3,32 +3,39 @@ set -eu
 
 # Assumes you've set [client] user and password in ~/.my.cnf
 
+################################################
 # You'll want to set these.
-SITE='gatoflow'
-SRC='prod'
-DST='staging'
-DST_DOMAIN="staging.${SITE}.com" # either staging or www
+SITE='helloworld'
 PARENT_DIR="/home/ubuntu/${SITE}"
+SRC='staging'
+DST='prod'
+SRC_HOSTNAME='staging.aliceinhelloworld.com'
+DST_HOSTNAME='www.aliceinhelloworld.com'
+################################################
 
-NEW_DOMAIN_SCRIPT='/home/ubuntu/wordpress-change-site-url/update_wp_db_new_domain.sh'
-
-if ! [ -f $NEW_DOMAIN_SCRIPT ]; then
-    echo "Requires NEW_DOMAIN_SCRIPT to be installed. Bailing now!"
+if [[ $SRC = $DST ]]; then
+    echo "Source and destination sites must be different: $SRC and $DST."
     exit 1
 fi
 
-echo "Taking the ${DST} site down, first."
+if [[ $SRC_HOSTNAME = $DST_HOSTNAME ]]; then
+    echo "Source and destination hostnames must be different: $SRC and $DST."
+    exit 1
+fi
+
+echo "Disabling apache site: ${SITE}_${DST}"
+sudo a2dissite ${SITE}_${DST}.conf && sudo service apache2 reload
+# TODO - After running a2dissite, if you navigate to the $DST site, you'll be redirected to
+#        another subdomain hosted on this server. For example, if you disable www.helloworld.com,
+#        you'll be redirected to www.someothersite.com that exists in /etc/apache2/sites-enabled.
+#        It would be nicer to instead get a "Site is down for maintenance, we'll be back shortly"
+#        message, or even just an HTTP 404.
+
+echo "Wiping the ${DST} webroot directory."
 sudo rm -rf $PARENT_DIR/webroot_${DST}
 
-echo "Dropping and re-creating ${DST} database, to prevent extra tables from sticking around."
+echo "Dropping ${DST} database completely, to prevent extra tables from sticking around."
 echo "drop database wp_${SITE}_${DST}" | mysql
-echo "create database wp_${SITE}_${DST}" | mysql
-
-echo "Dumping and loading from ${SRC} -> ${DST} database."
-mysqldump wp_${SITE}_${SRC} | mysql wp_${SITE}_${DST}
-
-echo "Updating Site URL in database."
-$NEW_DOMAIN_SCRIPT wp_${SITE}_${DST} $DST_DOMAIN
 
 echo "Copying the webroot directory from ${SRC} -> ${DST}."
 sudo cp -r $PARENT_DIR/webroot_{$SRC,$DST}
@@ -36,5 +43,18 @@ sudo chown -R www-data:public_html $PARENT_DIR/webroot_${DST}
 
 echo "Pointing ${DST} wp-config.php to the ${DST} database."
 sudo sed -i "s/DB_NAME\(.*\)_${SRC}/DB_NAME\1_${DST}/" $PARENT_DIR/webroot_${DST}/wp-config.php
+
+echo "Dumping and loading database from ${SRC} -> ${DST} database."
+echo "create database wp_${SITE}_${DST}" | mysql
+mysqldump wp_${SITE}_${SRC} | mysql wp_${SITE}_${DST}
+
+echo "Updating Site URL in database."
+wp search-replace \
+    $SRC_HOSTNAME $DST_HOSTNAME \
+    --path="$PARENT_DIR/webroot_${DST}" \
+    --precise --all-tables --verbose
+
+echo "Enabling apache site: ${SITE}_${DST}"
+sudo a2ensite ${SITE}_${DST}.conf && sudo service apache2 reload
 
 echo "Successfully cloned from ${SRC} -> ${DST}."
